46352713407910cc6ee279e8c2e9855959baee07
Niraj Kumar <niraj.nijju@gmail.com>
2019-05-03 16:07:13 +0530

    Update remindRuchi.sh

--- remindRuchi.sh
+++ remindRuchi.sh
@@ -1,1 +1,19 @@
-/usr/local/bin/terminal-notifier  -message "miss me ?" -title "Ruchi" -sound "Submarine" 
+
+LINES=$(cat today.txt | wc -l)
+
+if [ $LINES == 0 ]
+then
+	LINES=$(cat msg.txt | wc -l)
+	R_LINE=$(($RANDOM % LINES))
+	msg=$(sed -n "${R_LINE}p" msg.txt)
+	echo ${R_LINE}
+else
+   msg=$(cat today.txt)
+fi
+
+
+
+echo "msg: ${msg}"
+
+
+/usr/local/bin/terminal-notifier  -message "${msg}" -title "R" -sound "Submarine" 

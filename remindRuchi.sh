71288f4f539d073f26ebee1b5624dd87195f6959
Niraj Kumar <niraj.nijju@gmail.com>
2019-05-03 16:23:33 +0530

    Update remindRuchi.sh

--- remindRuchi.sh
+++ remindRuchi.sh
@@ -1,12 +1,14 @@
 
+date
+
 LINES=$(cat today.txt | wc -l)
 
-if [ $LINES == 0 ]
+if [ $LINES < 2 ]
 then
 	LINES=$(cat msg.txt | wc -l)
 	R_LINE=$(($RANDOM % LINES))
 	msg=$(sed -n "${R_LINE}p" msg.txt)
-	echo ${R_LINE}
+	# echo ${R_LINE}
 else
    msg=$(cat today.txt)
 fi
@@ -16,4 +18,4 @@ fi
 echo "msg: ${msg}"
 
 
-/usr/local/bin/terminal-notifier  -message "${msg}" -title "R" -sound "Submarine" 
+/usr/local/bin/terminal-notifier  -message "${msg} \n remote" -title "R" -sound "Submarine" 
